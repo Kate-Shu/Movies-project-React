58ddd8cd6474e6c6c7d66f2a68f73ed8d2cf5749
Kate-Shu <k.onufrichuk@gmail.com>
2022-01-08 16:05:31 +0200

    project complete

--- movies-project-react/src/components/MovieList.jsx
+++ movies-project-react/src/components/MovieList.jsx
@@ -1,14 +1,16 @@
 import { Movie } from './Movie';
 
 function MovieList(props) {
-    const { movies } = props;
+    const { movies = [] } = props;
 
     return (
         <div className="movies">
-            {movies.map(
-                (movie) => (
-                    <Movie key={movie.imdbID} {...movie} />
-                ) //...movie= {title=movie.Title}, {poster=movie.Poster}
+            {movies.length ? (
+                movies.map(
+                    (movie) => <Movie key={movie.imdbID} {...movie} /> //...movie= {title=movie.Title}, {poster=movie.Poster}
+                )
+            ) : (
+                <h4>Nothing found</h4>
             )}
         </div>
     );
--- movies-project-react/src/layout/Header.jsx
+++ movies-project-react/src/layout/Header.jsx
@@ -1,11 +1,17 @@
-function Header(){
-  return <nav className='green darken-1'>
-  <div className="nav-wrapper">
-    <a href="!#" className="brand-logo">React Movies</a>
-    <ul id="nav-mobile" className="right hide-on-med-and-down">
-      <li><a href="!#">Repository</a></li>
-    </ul>
-  </div>
-</nav>
+function Header() {
+    return (
+        <nav className="green darken-1">
+            <div className="nav-wrapper">
+                <a href="!#" className="brand-logo">
+                    React Movies
+                </a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li>
+                        <a href="!#">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
-export {Header}
+export { Header };
